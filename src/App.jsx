5acd96e94e75185b23fc68dc6c0f3799d8afdbdf
Antonio Rios <sec98b4e@gmail.com>
2022-11-25 22:58:54 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Holamundo from "./components/Holamundo";
+
+function App() {
+  return (
+    <div className="App">
+      <Holamundo />
+      <Holamundo />
+      <Holamundo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Holamundo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Holamundo() {
+  return <div>Hola mundo!</div>;
+}
+
+export default Holamundo;
